5915d3b3618b881d6d4713dead168ef88f3e289a
xaterz <ruofan.yu@gmail.com>
2016-10-20 21:28:27 +0800

    Updated classifier

--- classifier/__init__.py
+++ classifier/__init__.py
@@ -10,10 +10,10 @@ def mySVM():
     os.chdir("../")
 
     print("Loading training data...")
-    mfccTrainData = np.loadtxt("feature/acoustic/train/mfccMean.csv", delimiter=",")
-    spectTrainData = np.loadtxt("feature/acoustic/train/spectMean.csv", delimiter=",")
-    zeroTrainData = np.loadtxt("feature/acoustic/train/zero.csv", delimiter=",")
-    energyTrainData = np.loadtxt("feature/acoustic/train/energy.csv", delimiter=",")
+    mfccTrainData = np.loadtxt("feature/acoustic/train_converted/mfccMean.csv", delimiter=",")
+    spectTrainData = np.loadtxt("feature/acoustic/train_converted/spectMean.csv", delimiter=",")
+    zeroTrainData = np.loadtxt("feature/acoustic/train_converted/zero.csv", delimiter=",")
+    energyTrainData = np.loadtxt("feature/acoustic/train_converted/energy.csv", delimiter=",")
     X_train = np.concatenate((mfccTrainData, spectTrainData, zeroTrainData, energyTrainData), axis=1)
     print(X_train.shape)
 
@@ -24,20 +24,35 @@ def mySVM():
     i = 0
     for line in fileIn:
         if i == numFiles: break
-        venues.append(int(line.split("\t")[1].replace("\n", "")))
+        lineSplit = line.split("\t")
+        venues.append(int(lineSplit[1].replace("\n", "")))
         i += 1
     fileIn.close()
     Y_train = np.transpose(np.asarray(venues))
-    #print(Y_train)
-    print(Y_train.shape)
-
+    
     print("Loading test data...")
-    mfccTestData = np.loadtxt("feature/acoustic/validate/mfccMean.csv", delimiter=",")
-    spectTestData = np.loadtxt("feature/acoustic/validate/spectMean.csv", delimiter=",")
-    zeroTestData = np.loadtxt("feature/acoustic/validate/zero.csv", delimiter=",")
-    energyTestData = np.loadtxt("feature/acoustic/validate/energy.csv", delimiter=",")
+    mfccTestData = np.loadtxt("feature/acoustic/validate_converted/mfccMean.csv", delimiter=",")
+    spectTestData = np.loadtxt("feature/acoustic/validate_converted/spectMean.csv", delimiter=",")
+    zeroTestData = np.loadtxt("feature/acoustic/validate_converted/zero.csv", delimiter=",")
+    energyTestData = np.loadtxt("feature/acoustic/validate_converted/energy.csv", delimiter=",")
     X_test = np.concatenate((mfccTestData, spectTestData, zeroTestData, energyTestData), axis=1)
     print(X_test.shape)
+
+    numFiles = X_test.shape[0]
+    fileIn = open("vine-venue-validation-orderByName.txt", "r")
+    audioFiles = []
+    venues = []
+    i = 0
+    for line in fileIn:
+        if i == numFiles: break
+        lineSplit = line.split("\t")
+        audioFiles.append(int(lineSplit[0]))
+        venues.append(int(lineSplit[1].replace("\n", "")))
+        i += 1
+    fileIn.close()
+
+    Y_files = np.transpose(np.asarray(audioFiles))
+    Y_truth = np.transpose(np.asarray(venues))
 
     print('Data Load Done.')
 
@@ -53,12 +68,16 @@ def mySVM():
     print("Predicting classes for test data...")
     Y_predicted = np.asarray(model.predict(X_test))
     #Y_scores = model.decision_function(X_test)
+        
     print('SVM Train Done.')
 
     print("Prediction result:")
     print(Y_predicted)
+    
     print("Saving result...")
-    np.savetxt("prediction.csv", Y_predicted, delimiter=",")
+    print Y_files.shape, Y_predicted.shape, Y_truth.shape
+    result = np.transpose(np.vstack((Y_files, Y_predicted, Y_truth)))
+    np.savetxt("prediction.csv", result, delimiter=",")
     #np.savetxt("prediction_scores.csv", Y_scores, delimiter=",")
     print("Done!")
 
