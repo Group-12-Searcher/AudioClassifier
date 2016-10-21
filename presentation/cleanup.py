1f6a981d663da08d3118381e179d3fdd5bb966df
nlzz22 <nlzz_22@hotmail.com>
2016-10-21 17:58:45 +0800

    Added cleanup code for presentation and added missing soundconverter file

---
+++ presentation/cleanup.py
@@ -0,0 +1,16 @@
+import os
+import glob
+
+print ("Removing csv files...")
+for csvFile in glob.glob("*.csv"):
+    os.remove(csvFile)
+print ("Removing wav files (original)...")
+for wavFile in glob.glob("*.wav"):
+    os.remove(wavFile)
+print ("Removing jpg frames...")
+for frames in glob.glob("frame/*.jpg"):
+    os.remove(frames)
+print ("Removing wav files (converted)...")
+for wavFile in glob.glob("converted/*.wav"):
+    os.remove(wavFile)
+print ("All cleanup done... Ready for presentation!")
