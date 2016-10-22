1b22fdde41c433bb476c3bbc6bb504ad8e3132c6
xaterz <ruofan.yu@gmail.com>
2016-10-22 16:32:09 +0800

    Finished group classifier

--- classifier/__init__.py
+++ classifier/__init__.py
@@ -31,23 +31,15 @@ def mySVM():
     #X_test = spectTestData
     print(X_test.shape)
 
-    '''
-    veryNoisy = [1,2,21,22,26]
+    veryNoisy = [1,21,22,26]
     sometimesNoisy = [3,4,9,14,20]
     notVeryNoisy = [5,6,8,19,28]
     mostlyQuiet = [11,16,24,29,30]
     sports = [10,12,13,15,23,27]
-    music = [7,17,18,25]
-    '''
-    veryNoisy = [1,2,21,22,26]
-    sometimesNoisy = [3,4,9,14,20]
-    notVeryNoisy = [5,6,8,19,28]
-    mostlyQuiet = [11,16,24,29,30]
-    sports = [10,12,13,15,23,27]
-    music = [7,17,18,25]
+    music = [2,7,17,18,25]
     categories = [veryNoisy, sometimesNoisy, notVeryNoisy, mostlyQuiet, sports, music]
-
-    numGroups = 6
+    
+    numGroups = len(categories)
     X_train_list = [[],[],[],[],[],[]]
     Y_train_list = [[],[],[],[],[],[]]
     X_test_list = [[],[],[],[],[],[]]
@@ -74,7 +66,6 @@ def mySVM():
 
     numFiles = X_test.shape[0]
     fileIn = open("vine-venue-validation-orderByName.txt", "r")
-    audioFiles = []
     venues = []
     i = 0
     for line in fileIn:
@@ -85,13 +76,9 @@ def mySVM():
         for j in range(numGroups):
             if v in categories[j]:
                 venues.append(j+1)
-                '''X_test_list[j].append(X_test[i])
-                Y_truth_list[j].append(v)
-                Y_files_list[j].append(f)'''
                 break
         i += 1
     fileIn.close()
-    #Y_files = np.transpose(np.asarray(audioFiles))
     Y_truth = np.transpose(np.asarray(venues))
 
     print('Data Load Done.')
@@ -106,10 +93,13 @@ def mySVM():
 
     print("Predicting group for test data...")
     Y_predicted = np.asarray(model.predict(X_test))
-    #Y_scores = model.decision_function(X_test)
+    grouphits = 0
+    for i in range(numFiles):
+        if Y_predicted[i] == Y_truth[i]:
+            grouphits += 1
     
     print('SVM Train Done.')
-    print("Prediction result:")
+    print("Classification report:")
     print(classification_report(Y_truth, Y_predicted))
 
     fileIn = open("vine-venue-validation-orderByName.txt", "r")
@@ -128,27 +118,56 @@ def mySVM():
     fileIn.close()
 
     totalhits = 0
+    topRankings = []
     for i in range(numGroups):
         print "Training classifier for group", i+1
+        if len(X_test_list[i]) == 0:
+            print "Group is empty!"
+            continue
+        
         model = svm.SVC(kernel='linear', C=0.0001, degree=3, shrinking=True, verbose=False)
         model.fit(X_train_list[i], Y_train_list[i])
+        model_linear = svm.LinearSVC(C=0.00001)
+        model_linear.fit(X_train_list[i], Y_train_list[i])
+        
         Y_predicted = np.asarray(model.predict(X_test_list[i]))
+        Y_scores = np.asarray(model_linear.decision_function(X_test_list[i]))
         print('SVM Train Done.')
-        print("Prediction result:")
-        print(classification_report(Y_truth_list[i], Y_predicted))
+        print("Classification report:")
+        print(classification_report(Y_truth_list[i], Y_predicted))         
 
         k = Y_predicted.shape[0]
         hit = 0
         for j in range(k):
             if Y_truth_list[i][j] == Y_predicted[j]:
                 hit += 1
+            ranking = []
+            ranking.append(int(Y_predicted[j]))
+
+            scores = Y_scores[j].tolist()
+            scores[categories[i].index(Y_predicted[j])] = -999
+            for m in range(len(categories[i])-1):
+                v = categories[i][scores.index(max(scores))]
+                ranking.append(v)
+                scores[categories[i].index(v)] = -999
+            print j+1, "- Top venues are:", ranking
+
+            while len(ranking) < numGroups:
+                ranking.append(0)
+            topRankings.append(ranking)
+            
         print "Number of hits:", hit, "/", k
         totalhits += hit
         
         print("Saving result...")
-        result = np.transpose(np.vstack((np.asmatrix(Y_files_list[i]), Y_predicted, np.asmatrix(Y_truth_list[i]))))
-        np.savetxt("prediction_group"+str(i+1)+".csv", result, delimiter=",")
+        prediction = np.transpose(np.vstack((np.asmatrix(Y_files_list[i]), Y_predicted, np.asmatrix(Y_truth_list[i]))))
+        np.savetxt("prediction_group"+str(i+1)+".csv", prediction, delimiter=",")
+        np.savetxt("prediction_group"+str(i+1)+"_scores.csv", Y_scores, delimiter=",")
+
+    np.savetxt("toprankings.csv", np.array(topRankings), delimiter=",")
+        
     print "\nGrand total number of hits:", totalhits, "/ 900"
+    print "Number of group hits:", grouphits, "/ 900"
     
     '''
     print("Saving result...")
